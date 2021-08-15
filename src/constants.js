58e7cb02de7adfe8cdf3f71ce2eed3f31b02297b
Harry <harendra.singh2015@vit.ac.in>
2021-08-15 22:21:02 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+	BASE_URL:
+		"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=fd076ceff64c2c4c9c9daa87c7f221d4&per_page=20",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
